376d59a195c17b4c6f853b36ce5fc1d4e9a52411
Ondřej Roztočil <roztocil@outlook.com>
2023-11-03 20:26:40 +0100

    Tiny ML tasks 5-7

--- Tiny.ML/Interpreter.fs
+++ Tiny.ML/Interpreter.fs
@@ -4,6 +4,8 @@ type Value =
     | ValNum of int
     | ValClosure of string * Expression * VariableContext
     | ValTuple of Value * Value
+    | ValCase of bool * Value
+    | ValUnit
 
 and Expression =
     | Constant of int
@@ -14,15 +16,20 @@ and Expression =
     | Application of Expression * Expression
     | Lambda of string * Expression
     | Let of string * Expression * Expression
+    | Recursive of string * Expression * Expression
     | Tuple of Expression * Expression
     | TupleGet of bool * Expression
+    | Case of bool * Expression
+    | Match of Expression * string * Expression * Expression
+    | Unit
 
-and VariableContext = Map<string, Value>
+and VariableContext = Map<string, Lazy<Value>>
 
 module Interpreter =
     let rec evaluate (ctx: VariableContext) expr =
         match expr with
         | Constant c -> ValNum(c)
+
         | Binary (op, left, right) ->
             let leftVal = evaluate ctx left
             let rightVal = evaluate ctx right
@@ -34,7 +41,12 @@ module Interpreter =
                 | "*" -> ValNum(leftNum * rightNum)
                 | _ -> failwith "Unsupported operation"
             | _ -> failwith "Unsupported binary operation arguments"
-        | Variable (v) -> ctx.[v]
+
+        | Variable (varName) ->
+            match ctx.TryFind varName with
+            | Some result -> result.Value
+            | _ -> failwith $"Accessing unbound variable '{varName}'"
+
         | Unary (op, right) ->
             let rightVal = evaluate ctx right
 
@@ -44,12 +56,14 @@ module Interpreter =
                 | "-" -> ValNum(-rightNum)
                 | _ -> failwith "Unsupported operation"
             | _ -> failwith "Unsupported unary operation argument"
+
         | If (condExpr, thenExpr, elseExpr) ->
             let condVal = evaluate ctx condExpr
 
             match condVal with
             | ValNum (1) -> evaluate ctx thenExpr
             | _ -> evaluate ctx elseExpr
+
         | Lambda (varName, body) -> ValClosure(varName, body, ctx)
 
         | Application (leftExpr, rightExpr) ->
@@ -57,8 +71,42 @@ module Interpreter =
             let rightVal = evaluate ctx rightExpr
 
             match leftVal with
-            | ValClosure (varName, body, closureCtx) -> evaluate (Map.add varName rightVal closureCtx) body
+            | ValClosure (varName, body, closureCtx) -> evaluate (Map.add varName (lazy rightVal) closureCtx) body
             | _ -> failwith "Left argument of application must be closure"
+
         | Let (varName, valueExpr, inExpr) ->
-            let value = evaluate ctx valueExpr
+            let value = lazy (evaluate ctx valueExpr)
             evaluate (Map.add varName value ctx) inExpr
+
+        | Recursive (funcName, lambdaExpr, inExpr) ->
+            match lambdaExpr with
+            | Lambda(varName, body) ->
+                let rec lambdaVal = ValClosure(varName, body, Map.add funcName (lazy lambdaVal) ctx)
+                evaluate (Map.add funcName (lazy lambdaVal) ctx) inExpr
+            | _ -> failwith "Second argument of Recursive must be a lambda"
+
+        | Tuple (firstExpr, secondExpr) ->
+            let firstVal = evaluate ctx firstExpr
+            let secondVal = evaluate ctx secondExpr
+            ValTuple(firstVal, secondVal)
+
+        | TupleGet (wantFirst, expr) ->
+            let value = evaluate ctx expr
+
+            match wantFirst, value with
+            | true, ValTuple (first, _) -> first
+            | false, ValTuple (_, second) -> second
+            | _ -> failwith "TupleGet argument is not a tuple"
+
+        | Case (isFirst, expr) ->
+            let value = evaluate ctx expr
+            ValCase(isFirst, value)
+
+        | Match (matchedExpr, varName, firstExpr, secondExpr) ->
+            let matchedVal = evaluate ctx matchedExpr
+
+            match matchedVal with
+            | ValCase (true, value) -> evaluate (Map.add varName (lazy value) ctx) firstExpr
+            | ValCase (false, value) -> evaluate (Map.add varName (lazy value) ctx) secondExpr
+            | _ -> failwith "Matched value is not a case value"
+        | Unit -> ValUnit
