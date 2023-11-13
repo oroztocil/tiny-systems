da0510bd343f240c97d2e7855b66f1d1faa19607
Ondřej Roztočil <roztocil@outlook.com>
2023-11-13 17:41:15 +0100

    Add Tiny HM solutions 1-4

--- Tiny.HM/01-numerical-solver.fsx
+++ Tiny.HM/01-numerical-solver.fsx
@@ -3,68 +3,66 @@
 // ----------------------------------------------------------------------------
 
 type Number =
-  | Zero
-  | Succ of Number
-  | Variable of string
+    | Zero
+    | Succ of Number
+    | Variable of string
 
 
-// NOTE: The four functions below currently return a wrong 
+// NOTE: The four functions below currently return a wrong
 // result, but one that makes the code run. As you implement
 // them (one by one), the tests should graudally start working.
 
 
-let rec occursCheck (v:string) (n:Number) = 
-  // TODO: Check if variable 'v' appears anywhere inside 'n'
-  false
+let rec occursCheck (v: string) (n: Number) =
+    match n with
+    | Variable(v2) -> v = v2
+    | Succ(n2) -> occursCheck v n2
+    | Zero -> false
 
-let rec substite (v:string) (subst:Number) (n:Number) =
-  // TODO: Replace all occurrences of variable 'v' in the
-  // number 'n' with the replacement number 'subst'
-  n
+let rec substitute (v: string) (subst: Number) (n: Number) =
+    match n with
+    | Zero -> Zero
+    | Succ n2 -> Succ(substitute v subst n2)
+    | Variable v2 when v = v2 -> subst
+    | _ -> n
 
-let substituteConstraints (v:string) (subst:Number) (constraints:list<Number * Number>) = 
-  // TODO: Substitute 'v' for 'subst' (use 'substitute') in 
-  // all numbers in all the constraints in 'constraints'
-  constraints
+let substituteConstraints (v: string) (subst: Number) (constraints: list<Number * Number>) =
+    // TODO: Substitute 'v' for 'subst' (use 'substitute') in
+    // all numbers in all the constraints in 'constraints'
+    List.map (fun (l, r) -> (substitute v subst l, substitute v subst r)) constraints
 
-let substituteAll (subst:list<string * Number>) (n:Number) =
-  // TODO: Perform all substitutions 
-  // specified  in 'subst' on the number 'n'
-  n
+let substituteAll (subst: list<string * Number>) (n: Number) =
+    List.fold (fun n2 (v, s) -> substitute v s n2) n subst
 
-let rec solve constraints = 
-  match constraints with 
-  | [] -> []
-  | (Succ n1, Succ n2)::constraints ->
-      solve ((n1, n2)::constraints)
-  | (Zero, Zero)::constraints -> solve constraints
-  | (Succ _, Zero)::_ | (Zero, Succ _)::_ -> 
-      failwith "Cannot be solved"
-  | (n, Variable v)::constraints | (Variable v, n)::constraints ->
-      if occursCheck v n then failwith "Cannot be solved (occurs check)"
-      let constraints = substituteConstraints v n constraints
-      let subst = solve constraints
-      let n = substituteAll subst n
-      (v, n)::subst
+let rec solve constraints =
+    match constraints with
+    | [] -> []
+    | (Succ n1, Succ n2) :: constraints -> solve ((n1, n2) :: constraints)
+    | (Zero, Zero) :: constraints -> solve constraints
+    | (Succ _, Zero) :: _
+    | (Zero, Succ _) :: _ -> failwith "Cannot be solved"
+    | (n, Variable v) :: constraints
+    | (Variable v, n) :: constraints ->
+        if occursCheck v n then
+            failwith "Cannot be solved (occurs check)"
+
+        let constraints = substituteConstraints v n constraints
+        let subst = solve constraints
+        let n = substituteAll subst n
+        (v, n) :: subst
 
 // Should work: x = Zero
-solve 
-  [ Succ(Variable "x"), Succ(Zero) ]
+solve [ Succ(Variable "x"), Succ(Zero) ]
 
-// Should faild: S(Z) <> Z
-solve 
-  [ Succ(Succ(Zero)), Succ(Zero) ]
+// // Should faild: S(Z) <> Z
+// solve
+//   [ Succ(Succ(Zero)), Succ(Zero) ]
 
 // Not done: Need to substitute x/Z in S(x)
-solve 
-  [ Succ(Variable "x"), Succ(Zero)
-    Variable "y", Succ(Variable "x") ]
+solve [ Succ(Variable "x"), Succ(Zero); Variable "y", Succ(Variable "x") ]
 
 // Not done: Need to substitute z/Z in S(S(z))
-solve 
-  [ Variable "x", Succ(Succ(Variable "z"))
-    Succ(Variable "z"), Succ(Zero) ]
+solve [ Variable "x", Succ(Succ(Variable "z")); Succ(Variable "z"), Succ(Zero) ]
 
 // Not done: Need occurs check
-solve
-  [ Variable "x", Succ(Variable "x") ]
+solve [ Variable "x", Succ(Variable "x") ]
--- Tiny.HM/04-more-expressions.fsx
+++ Tiny.HM/04-more-expressions.fsx
@@ -2,44 +2,74 @@
 // Type inference for binary operators and conditionals
 // ----------------------------------------------------------------------------
 
-type Expression = 
-  | Constant of int
-  | Binary of string * Expression * Expression
-  | If of Expression * Expression * Expression
-  | Variable of string
-  // NOTE: Added three more kinds of expression from TinyML
-  | Application of Expression * Expression
-  | Lambda of string * Expression
-  | Let of string * Expression * Expression
+type Expression =
+    | Constant of int
+    | Binary of string * Expression * Expression
+    | If of Expression * Expression * Expression
+    | Variable of string
+    // NOTE: Added three more kinds of expression from TinyML
+    | Application of Expression * Expression
+    | Lambda of string * Expression
+    | Let of string * Expression * Expression
 
-type Type = 
-  | TyVariable of string
-  | TyBool 
-  | TyNumber 
-  | TyList of Type
-  // NOTE: Added type for functions (of single argument)
-  | TyFunction of Type * Type
+type Type =
+    | TyVariable of string
+    | TyBool
+    | TyNumber
+    | TyList of Type
+    // NOTE: Added type for functions (of single argument)
+    | TyFunction of Type * Type
 
 // ----------------------------------------------------------------------------
 // Constraint solving
 // ----------------------------------------------------------------------------
 
-let rec occursCheck vcheck ty = 
-  // TODO: Add case for 'TyFunction' (need to check both nested types)
-  failwith "not implemented"
+let rec occursCheck (vcheck: string) (ty: Type) =
+    "CHECKING " |> System.Console.Write
+    vcheck |> System.Console.Write
+    " IN " |> System.Console.Write
+    ty |> System.Console.WriteLine
+    match ty with
+    | TyVariable(v) -> v = vcheck
+    | TyList(listType) -> occursCheck vcheck listType
+    | TyFunction(argType, retType) -> (occursCheck vcheck argType) || (occursCheck vcheck retType)
+    | _ -> false
 
-let rec substType (subst:Map<_, _>) t1 = 
-  // TODO: Add case for 'TyFunction' (need to substitute in both nested types)
-  failwith "not implemented"
+let rec substituteAll (subst: Map<string, Type>) ty =
+    match ty with
+    | TyVariable(varName) when subst.ContainsKey varName -> subst.[varName]
+    | TyList(listType) -> TyList(substituteAll subst listType)
+    // TODO: Add case for 'TyFunction' (need to substitute in both nested types)
+    | TyFunction(argType, retType) -> TyFunction(substituteAll subst argType, substituteAll subst retType)
+    | _ -> ty
 
-let substConstrs subst cs = 
-  failwith "implemented in step 2"
- 
-let rec solve constraints =
-  // TODO: Add case matching TyFunction(ta1, tb1) and TyFunction(ta2, tb2)
-  // This generates two new constraints, equating the argument/return types.
-  failwith "not implemented"
+let substituteConstraints (subst: Map<string, Type>) (cs: list<Type * Type>) =
+    List.map (fun (l, r) -> (substituteAll subst l, substituteAll subst r)) cs
 
+
+let rec solve cs =
+    match cs with
+    | [] -> []
+    | (TyNumber, TyNumber) :: cs -> solve cs
+    | (TyBool, TyBool) :: cs -> solve cs
+    | (TyList(t1), TyList(t2)) :: cs -> solve ((t1, t2) :: cs)
+    | (t, TyVariable v) :: cs
+    | (TyVariable v, t) :: cs ->
+        if occursCheck v t then
+            failwith "Does not type check (occurs check)"
+
+        let cs = substituteConstraints (Map.ofList [ v, t ]) cs
+        let subst = solve cs
+        let t = substituteAll (Map.ofList subst) t
+        (v, t) :: subst
+    | (TyFunction(argType1, retType1), TyFunction(argType2, retType2)) :: cs ->
+        // if occursCheck v t then
+        //     failwith "Does not type check (occurs check)"
+
+        // if occursCheck v t then
+        //     failwith "Does not type check (occurs check)"
+        solve ((argType1, argType2) :: (retType1, retType2) :: cs)
+    | _ -> failwith "Does not type check (unsatisfiable constraint)"
 
 // ----------------------------------------------------------------------------
 // Constraint generation & inference
@@ -49,90 +79,122 @@ type TypingContext = Map<string, Type>
 
 // NOTE: You will need this helper in checking of Lambda and Application.
 // It generates a new type variable each time you call 'newTypeVariable()'
-let newTyVariable = 
-  let mutable n = 0
-  fun () -> n <- n + 1; TyVariable(sprintf "_a%d" n)
+let newTyVariable =
+    let mutable n = 0
 
-let rec generate (ctx:TypingContext) e = 
-  match e with 
-  | Constant _ -> failwith "implemented in step 3"
-  | Binary("+", e1, e2) -> failwith "implemented in step 3"
-  | Binary("=", e1, e2) -> failwith "implemented in step 3"
-  | Binary(op, _, _) -> failwith "implemented in step 3"
-  | Variable v -> failwith "implemented in step 3"
-  | If(econd, etrue, efalse) -> failwith "implemented in step 3"
+    fun () ->
+        n <- n + 1
+        TyVariable(sprintf "_a%d" n)
 
-  | Let(v, e1, e2) ->
-      // TODO: Generate type & constraints for 'e1' first and then
-      // add the generated type to the typing context for 't2'.
-      failwith "not implemented"
-  
-  | Lambda(v, e) ->
-      let targ = newTyVariable()
-      // TODO: We do not know what the type of the variable 'v' is, so we 
-      // generate a new type variable and add that to the 'ctx'. The
-      // resulting type will be 'TyFunction' with 'targ' as argument type.
-      failwith "not implemented"
+let rec generate (ctx: TypingContext) e =
+    match e with
+    | Constant _ -> TyNumber, []
 
-  | Application(e1, e2) -> 
-      // TODO: Tricky case! We cannot inspect the generated type of 'e1'
-      // to see what the argument/return type of the function is. Instead,
-      // we have to generate a new type variable and add a constraint.
-      failwith "not implemented"
-  
+    | Binary("+", e1, e2)
+    | Binary("*", e1, e2) ->
+        let t1, s1 = generate ctx e1
+        let t2, s2 = generate ctx e2
+        let cs = s1 @ s2 @ [ t1, TyNumber; t2, TyNumber ]
+        TyNumber, cs
+
+    | Binary("=", e1, e2) ->
+        let t1, s1 = generate ctx e1
+        let t2, s2 = generate ctx e2
+        let cs = s1 @ s2 @ [ t1, TyNumber; t2, TyNumber ]
+        TyBool, cs
+
+    | Binary(op, _, _) -> failwithf "Binary operator '%s' not supported." op
+
+    | Variable v -> ctx.[v], []
+
+    | If(eCond, eThen, eElse) ->
+        let tCond, sCond = generate ctx eCond
+        let tThen, sThen = generate ctx eThen
+        let tElse, sElse = generate ctx eElse
+        let cs = sCond @ sThen @ sElse @ [ tCond, TyBool; tThen, tElse ]
+        TyNumber, cs
+
+    | Let(v, e1, e2) ->
+        let t1, s1 = generate ctx e1
+        let ctx = Map.add v t1 ctx
+        let t2, s2 = generate ctx e2
+        let cs = s1 @ s2
+        t2, cs
+
+    | Lambda(v, e) ->
+        let tArg = newTyVariable ()
+        let ctx = Map.add v tArg ctx
+        let tRet, sRet = generate ctx e
+        TyFunction(tArg, tRet), sRet
+
+    | Application(e1, e2) ->
+        // TODO: Tricky case! We cannot inspect the generated type of 'e1'
+        // to see what the argument/return type of the function is. Instead,
+        // we have to generate a new type variable and add a constraint.
+        let tLam, sLam = generate ctx e1
+        let tApp, sApp = generate ctx e2
+        let tRet = newTyVariable ()
+        let cs = sLam @ sApp @ [ tLam, TyFunction(tApp, tRet) ]
+        tRet, cs
+
 
 // ----------------------------------------------------------------------------
 // Putting it together & test cases
 // ----------------------------------------------------------------------------
 
 // Run both of the phases and return the resulting type
-let infer e = 
-  let typ, constraints = generate Map.empty e 
-  let subst = solve constraints
-  let typ = substType (Map.ofList subst) typ
-  typ
+let infer e =
+    let typ, constraints = generate Map.empty e
+    "RESULT:" |> System.Console.WriteLine
+    "TYPE: " |> System.Console.Write
+    typ |> System.Console.WriteLine
+    "CONSTRAINTS: " |> System.Console.Write
+    constraints |> System.Console.WriteLine
+    "SUBSTITUTIONS: " |> System.Console.Write
+    let subst = solve constraints
+    subst |> System.Console.WriteLine
+    "EXPR TYPE: " |> System.Console.Write
+    let typ = substituteAll (Map.ofList subst) typ
+    typ
 
 
 // NOTE: Using the above, you will end up with ugly random type variable
 // names like '_a4' etc. You can improve this by collecting all the type
-// variable names that appear in a type and substituting them with a 
+// variable names that appear in a type and substituting them with a
 // list of nice names. Useful bit of code to generate the substitution is:
 //
-//   Map.ofList [ for i, n in Seq.indexed ["_a4"; "_a5"] -> 
+//   Map.ofList [ for i, n in Seq.indexed ["_a4"; "_a5"] ->
 //     n, string('a' + char i) ]
 //
 // You would still need to write code to collect all type variables in a type.
 
 
 // let x = 10 in x = 10
-Let("x", Constant 10, Binary("=", Variable "x", Constant 10))
-|> infer 
+Let("x", Constant 10, Binary("=", Variable "x", Constant 10)) |> infer
 
 // let f = fun x -> x*2 in (f 20) + (f 1)
-Let("f",
-  Lambda("x", Binary("*", Variable("x"), Constant(2))),
-  Binary("+", 
-    Application(Variable("f"), Constant(20)),
-    Application(Variable("f"), Constant(1)) 
-  ))
+Let(
+    "f",
+    Lambda("x", Binary("*", Variable("x"), Constant(2))),
+    Binary("+", Application(Variable("f"), Constant(20)), Application(Variable("f"), Constant(1)))
+)
 |> infer
 
 // fun x f -> f (f x)
-Lambda("x", Lambda("f", 
-  Application(Variable "f", Application(Variable "f", Variable "x"))))
+Lambda("x", Lambda("f", Application(Variable "f", Application(Variable "f", Variable "x"))))
 |> infer
 
-// fun f -> f f 
+Application(Lambda("x", Lambda("f", Application(Variable "f", Application(Variable "f", Variable "x")))), Constant(1))
+|> infer
+
+// fun f -> f f
 // This does not type check due to occurs check
-Lambda("f", 
-  Application(Variable "f", Variable "f"))
-|> infer
+Lambda("f", Application(Variable "f", Variable "f")) |> infer
 
-// fun f -> f 1 + f (2 = 3) 
+// fun f -> f 1 + f (2 = 3)
 // This does not type check because argument of 'f' cannot be both 'int' and 'bool'
-Lambda("f", 
-  Binary("+",
-    Application(Variable "f", Constant 1),
-    Application(Variable "f", Binary("=", Constant 2, Constant 3))
-  ))
+Lambda(
+    "f",
+    Binary("+", Application(Variable "f", Constant 1), Application(Variable "f", Binary("=", Constant 2, Constant 3)))
+)
 |> infer
